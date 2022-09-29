041bf6cd5dfff2933cffc35201cf1930439edd19
gcvictor22 <victorgonzaleztr22@gmail.com>
2022-09-29 02:12:31 +0200

    First commit

---
+++ ejercicio1/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormComponent } from './components/FormularioRegistroComponent/form.component';
+import { MaterialImportsModule } from './material-imports/material-imports.module';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialImportsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ejercicio1/src/app/components/FormularioRegistroComponent/form.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, Validators} from '@angular/forms';
+import {FloatLabelType} from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  hideRequiredControl = new FormControl(false);
+  floatLabelControl = new FormControl('auto' as FloatLabelType);
+  options = this._formBuilder.group({
+    hideRequired: this.hideRequiredControl,
+    floatLabel: this.floatLabelControl,
+  });
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  getFloatLabelValue(): FloatLabelType {
+    return this.floatLabelControl.value || 'auto';
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Email no válido' : '';
+  }
+
+  nombre='';
+  apellidos='';
+  dni='';
+  sexo='';
+  gmail='';
+  telefono='';
+  conocer='';
+
+  enviarDatos() {
+    console.log(this.nombre)
+    console.log(this.apellidos)
+    console.log(this.dni)
+    console.log(this.sexo)
+    console.log(this.gmail)
+    console.log(this.telefono)
+    console.log(this.conocer)
+  }
+}
