67d10e6774af6476872bb748634d8a4fcc0b0d0d
gcvictor22 <victorgonzaleztr22@gmail.com>
2022-10-06 00:57:25 +0200

    no esta terminado

--- ejercicioPokemon/src/app/components/pokemon-list/pokemon-list.component.ts
+++ ejercicioPokemon/src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Pokemon } from 'src/app/interfaces/pokemon-response.interface';
+import { Pokemon, PokemonResponse } from 'src/app/interfaces/pokemon-response.interface';
 import { PokemonService } from 'src/app/service/pokemon.service';
 
+const URL_IMAGEN = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
@@ -9,6 +10,8 @@ import { PokemonService } from 'src/app/service/pokemon.service';
 })
 export class PokemonListComponent implements OnInit {
   listadoPokemon: Pokemon[] = []
+  extension = '.png'
+  img = URL_IMAGEN
 
   constructor(private pokemonService: PokemonService) { }
 
