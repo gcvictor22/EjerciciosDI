72b80c070aec64736488457559065b9d5fc66d21
gcvictor22 <victorgonzaleztr22@gmail.com>
2022-10-06 21:12:30 +0200

    Ejercicios terminados

--- ejercicioStarWars/src/app/components/people-list-component/people-list-component.component.ts
+++ ejercicioStarWars/src/app/components/people-list-component/people-list-component.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { People } from 'src/app/interfaces/people-list.interface';
+import { Peliculas } from 'src/app/interfaces/film-list.interface';
+import { People, PeopleResponse } from 'src/app/interfaces/people-list.interface';
 import { PeopleServiceService } from 'src/app/service/people-service.service';
 
+const URL_IMAGEN = 'https://starwars-visualguide.com/assets/img/characters/'
 @Component({
   selector: 'app-people-list-component',
   templateUrl: './people-list-component.component.html',
@@ -9,13 +11,18 @@ import { PeopleServiceService } from 'src/app/service/people-service.service';
 })
 export class PeopleListComponentComponent implements OnInit {
   peopleListado: People[] = []
-
+  filmsListado: Peliculas[] = []
+  url = URL_IMAGEN
 
   constructor(private peopleService: PeopleServiceService) { }
 
   ngOnInit(): void {
     this.peopleService.peopleList().subscribe(response => {
       this.peopleListado = response.results
+    })
+
+    this.peopleService.filmList().subscribe(response => {
+      this.filmsListado = response.results
     })
   }
 
--- ejercicioStarWars/src/app/service/people-service.service.ts
+++ ejercicioStarWars/src/app/service/people-service.service.ts
@@ -1,10 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { FilmResponse } from '../interfaces/film-list.interface';
 import { PeopleResponse } from '../interfaces/people-list.interface';
 
 const API_BASE_URL = 'https://swapi.dev/api'
-
 @Injectable({
   providedIn: 'root'
 })
@@ -18,5 +18,13 @@ export class PeopleServiceService {
 
   public getPeople(id: string) {
     return this.http.get(`${API_BASE_URL}/people`)
+  }
+
+  public filmList(): Observable<FilmResponse>{
+    return this.http.get<FilmResponse>(`${API_BASE_URL}/films`)
+  }
+
+  public getFilm(id: string) {
+    return this.http.get(`${API_BASE_URL}/films`)
   }
 }
