8ae7e02087e5c6dfde119fbb18f1febfea6e7c39
gcvictor22 <victorgonzaleztr22@triana.salesianos.edu>
2022-10-07 12:49:33 +0200

    ...

--- ejercicioPokemon/src/app/components/pokemon-list/pokemon-list.component.ts
+++ ejercicioPokemon/src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Type } from '@angular/core';
+import { DetailsResponse, Stat, Tipo } from 'src/app/interfaces/pokemon-details.interface';
 import { Pokemon, PokemonResponse } from 'src/app/interfaces/pokemon-response.interface';
 import { PokemonService } from 'src/app/service/pokemon.service';
 
@@ -9,7 +10,11 @@ const URL_IMAGEN = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/spr
   styleUrls: ['./pokemon-list.component.css']
 })
 export class PokemonListComponent implements OnInit {
+
   listadoPokemon: Pokemon[] = []
+  listadoTipos: Tipo [] = [] 
+  listadoHP: Stat[] = []
+  listadoDetailResponse : DetailsResponse [] = []
   img = URL_IMAGEN
   nombreBuscador=''
 
@@ -19,6 +24,23 @@ export class PokemonListComponent implements OnInit {
     this.pokemonService.pokemonList().subscribe(response => {
       this.listadoPokemon = response.results;
     })
+
+    this.pokemonService.detailList().subscribe(response =>{
+      this.listadoTipos = response.types;
+    })
+
+    this.pokemonService.detailList().subscribe(response =>{
+      this.listadoHP = response.stats;
+    })
+  }
+
+  getPokemonId(pokemon: Pokemon){
+    return pokemon.url.split('/')[6];
+  }
+
+  getPokemonUrl(pokemon: Pokemon) {
+    let id =this.getPokemonId(pokemon);
+    return 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+id+'.png'
   }
 
 }
--- ejercicioPokemon/src/app/service/pokemon.service.ts
+++ ejercicioPokemon/src/app/service/pokemon.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { DetailsResponse } from '../interfaces/pokemon-details.interface';
 import { PokemonResponse } from "../interfaces/pokemon-response.interface";
 
-const API_BASE_URL = 'https://pokeapi.co/api/v2/pokemon?&limit=500'
+const API_BASE_URL = 'https://pokeapi.co/api/v2/pokemon?&limit=504'
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +18,14 @@ export class PokemonService {
   }
 
   public getPokemon(id: string) {
+    return this.http.get(`${API_BASE_URL}/pokemon`)
+  }
+
+  public detailList(): Observable<DetailsResponse>{
+    return this.http.get<DetailsResponse>(`${API_BASE_URL}/pokemon`)
+  }
+
+  public getDetail(id: string){
     return this.http.get(`${API_BASE_URL}/pokemon`)
   }
 }
