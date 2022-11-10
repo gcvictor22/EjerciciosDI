8703f383078bdf5048e0ea45468708a8e948c78e
gcvictor22 <victorgonzaleztr22@gmail.com>
2022-11-10 23:17:22 +0100

    ...

--- laGasolina2/src/app/components/fuel-stations/fuel-stations.component.ts
+++ laGasolina2/src/app/components/fuel-stations/fuel-stations.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FuelStation } from 'src/app/interfaces/fuelStation.interface';
+import { Province } from 'src/app/interfaces/provinces.interface';
 import { FuelStationsService } from 'src/app/services/fuel-stations.service';
 
 @Component({
@@ -14,82 +15,97 @@ export class FuelStationsComponent implements OnInit {
   priceSelect = '';
   fuelType: FuelStation = {} as FuelStation;
   fuelTypeValue: keyof typeof this.fuelType = 'Precio Gasolina 95 E5'
-  fuelName : string[] = ['-', 'Biodesel', 'Bioetanol', 'Gas Natural Comprimido', 'Gas Natural Licuado', 'Gases Licuados del Petróleo',
+  fuelName : string[] = ['Biodesel', 'Bioetanol', 'Gas Natural Comprimido', 'Gas Natural Licuado', 'Gases Licuados del Petróleo',
   'Gasóleo A', 'Gasóleo B', 'Gasóleo Premium', 'Gasolina 95 E10', 'Gasolina 95 E5', 'Gasolina 95 E5 Premium', 'Gasolina 98 E10', 'Gasolina 98 E5', 'Hidrógeno']
+  provincesList: Province[] = [];
 
   constructor(private fuelStationService: FuelStationsService) {}
 
   ngOnInit(): void {
+    this.fuelStationService.getAllProvinces().subscribe(resp => {
+      this.provincesList = resp;
+    });
+  }
 
+  changeFuelType(type: keyof typeof this.fuelType = 'Precio Gasolina 95 E5'){
     this.fuelStationService.getAllFuelStations().subscribe((resp) => {
-      this.fuelStationList = resp.ListaEESSPrecio.filter(fuelS => fuelS[this.fuelTypeValue] != '')
+      this.fuelStationList = resp.ListaEESSPrecio.filter(fuelS => fuelS[type] != '');
 
       this.fuelStationList.sort((a, b) => {
-        if (a[this.fuelTypeValue] > b[this.fuelTypeValue]) {
+        if (a[type] > b[type]) {
           return 1;
-        } else if (a[this.fuelTypeValue] < b[this.fuelTypeValue]) {
+        } else if (a[type] < b[type]) {
           return -1;
         } else {
           return 0;
         }
       });
 
-      this.minPrice = this.fuelStationList[0][this.fuelTypeValue].split(',')[0]+('.'+this.fuelStationList[0][this.fuelTypeValue].split(',')[1])
-      this.maxPrice = this.fuelStationList.reverse()[0][this.fuelTypeValue].split(',')[0]+('.'+this.fuelStationList[0][this.fuelTypeValue].split(',')[1])
-      this.priceSelect = this.maxPrice
-
-      this.fuelStationList.reverse();
+      this.minPrice = this.fuelStationList[0][type].split(',')[0]+('.'+this.fuelStationList[0][type].split(',')[1]);
+      this.maxPrice = this.fuelStationList.reverse()[0][type].split(',')[0]+('.'+this.fuelStationList[0][type].split(',')[1]);
+      this.priceSelect = this.maxPrice;
       
+      this.fuelStationList.reverse();
     });
-
-    console.log(this.fuelName.indexOf('Biodesel'));
-    
-    
   }
 
   selectTypeOfFuel(str:string){
     switch (str) {
       case 'Biodesel':
         this.fuelTypeValue = "Precio Biodiesel";
+        this.changeFuelType(this.fuelTypeValue);
         break;
       case 'Bioetanol':
         this.fuelTypeValue = "Precio Bioetanol";
+        this.changeFuelType(this.fuelTypeValue);
         break;
       case 'Gas Natural Comprimido':
         this.fuelTypeValue = "Precio Gas Natural Comprimido";
+        this.changeFuelType(this.fuelTypeValue);
         break;
       case 'Gas Natural Licuado':
         this.fuelTypeValue = "Precio Gas Natural Licuado";
+        this.changeFuelType(this.fuelTypeValue);
         break;
       case 'Gases Licuados del Petróleo':
         this.fuelTypeValue = "Precio Gases licuados del petróleo";
+        this.changeFuelType(this.fuelTypeValue);
         break;
       case 'Gasóleo A':
         this.fuelTypeValue = "Precio Gasoleo A";
+        this.changeFuelType(this.fuelTypeValue);
         break;
       case 'Gasóleo B':
         this.fuelTypeValue = "Precio Gasoleo B";
+        this.changeFuelType(this.fuelTypeValue);
         break;
       case 'Gasóleo Premium':
         this.fuelTypeValue = "Precio Gasoleo Premium";
+        this.changeFuelType(this.fuelTypeValue);
         break;
       case 'Gasolina 95 E10':
         this.fuelTypeValue = "Precio Gasolina 95 E10";
+        this.changeFuelType(this.fuelTypeValue);
         break;
       case 'Gasolina 95 E5':
         this.fuelTypeValue = "Precio Gasolina 95 E5";
+        this.changeFuelType(this.fuelTypeValue);
         break;
       case 'Gasolina 95 E5 Premium':
         this.fuelTypeValue = "Precio Gasolina 95 E5 Premium";
+        this.changeFuelType(this.fuelTypeValue);
         break;
       case 'Gasolina 98 E10':
         this.fuelTypeValue = "Precio Gasolina 98 E10";
+        this.changeFuelType(this.fuelTypeValue);
         break;
       case 'Gasolina 98 E5':
         this.fuelTypeValue = "Precio Gasolina 98 E5";
+        this.changeFuelType(this.fuelTypeValue);
         break;
       case 'Hidrógeno':
         this.fuelTypeValue = "Precio Hidrogeno";
+        this.changeFuelType(this.fuelTypeValue);
         break;
     }
   }
--- laGasolina2/src/app/services/fuel-stations.service.ts
+++ laGasolina2/src/app/services/fuel-stations.service.ts
@@ -2,8 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { FuelStationResponse } from '../interfaces/fuelStation.interface';
+import { Province } from '../interfaces/provinces.interface';
+
+//https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/
 
 const API_BASE_URL = 'https://raw.githubusercontent.com/gcvictor22/LohCangri/main/fuelstationlist4everyone.json'
+const API_PROVINCES_URL = 'https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/Listados/Provincias'
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +17,11 @@ export class FuelStationsService {
   constructor(private http : HttpClient) { }
 
   public getAllFuelStations():Observable<FuelStationResponse>{
-    return this.http.get<FuelStationResponse>(`${API_BASE_URL}`)
+    return this.http.get<FuelStationResponse>(`${API_BASE_URL}`);
   }
+
+  public getAllProvinces():Observable<Province[]>{
+    return this.http.get<Province[]>(`${API_PROVINCES_URL}`);
+  }
+
 }
