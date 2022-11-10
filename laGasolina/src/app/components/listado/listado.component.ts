d8a9f557342aafbc0d3429632f8ecc63a11759d7
gcvictor22 <victorgonzaleztr22@triana.salesianos.edu>
2022-11-10 12:26:55 +0100

    ...

--- laGasolina/src/app/components/listado/listado.component.ts
+++ laGasolina/src/app/components/listado/listado.component.ts
@@ -20,8 +20,18 @@ export class ListadoComponent implements OnInit {
   }
 
   getGasolineraImg(gas : Gasolinera){
-    if (gas['Rótulo'] == 'REPSOL') {
-      //https://1000marcas.net/wp-content/uploads/2020/11/Repsol-Logo.png
+    if (gas['Rótulo'] === 'REPSOL') {
+      return 'https://1000marcas.net/wp-content/uploads/2020/11/Repsol-Logo.png'
+    }else if(gas['Rótulo'] === 'CARREFOUR'){
+      return 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Carrefour_logo.svg/245px-Carrefour_logo.svg.png'
+    }else if(gas['Rótulo'] === 'CEPSA'){
+      return 'https://play-lh.googleusercontent.com/LKB7R0mxT48Hhc64pr78kxWKVHU5QPfa4hqa5iO4whm_PFxX9jPwlxDGfVwIPEKGGHE'
+    }else if(gas['Rótulo'] === 'BP ROMICA' || gas['Rótulo'] === 'BP'){
+      return 'https://media.cylex.es/companies/1327/0019/logo/logo44036998.jpg'
+    }else if(gas['Rótulo'] === 'PLENOIL'){
+      return 'https://pbs.twimg.com/profile_images/1053208937003208704/8kuLex7u_400x400.jpg'
+    }else{
+      return 'https://www.pnguniverse.com/wp-content/uploads/2020/09/Icono-gasolinera.png'
     }
   }
 
