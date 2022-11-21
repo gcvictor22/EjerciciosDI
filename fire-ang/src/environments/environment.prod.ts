a11cbd592b97f690fa060d0a10d66fa7b3e3c96d
gcvictor22 <victorgonzaleztr22@gmail.com>
2022-11-21 19:30:05 +0100

    ...

---
+++ fire-ang/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: 'AIzaSyBwAXrB74-PuW8CqmhX5dc3hoNQ33m1xDo',
+    authDomain: 'fir-angular-68574.firebaseapp.com',
+    projectId: 'fir-angular-68574',
+    storageBucket: 'fir-angular-68574.appspot.com',
+    messagingSenderId: '162242816219',
+    appId: '1:162242816219:web:818c3b4f6fdeeeccbac4fe',
+    measurementId: 'G-XGG6V79PEV'
+  },
+};
