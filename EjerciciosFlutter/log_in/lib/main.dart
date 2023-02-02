3bab4a48d59a335289c36f2e72fc4e7c0dda2c32
gcvictor22 <victorgonzaleztr22@gmail.com>
2023-02-02 19:37:44 +0100

    KLK pasa no lo se

--- EjerciciosFlutter/log_in/lib/main.dart
+++ EjerciciosFlutter/log_in/lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,92 +12,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      home: LoginPage(),
     );
   }
 }
 
 ////////////////////////////////////////
 
-class Login extends StatelessWidget {
-  const Login({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: NetworkImage(
-                  'https://img.theculturetrip.com/450x/smart/wp-content/uploads/2019/11/gettyimages-643821531.jpg'),
-              fit: BoxFit.cover)),
-      child: Center(
-          heightFactor: 18,
-          child: Column(
-            children: [
-              const Text('\n\n'),
-              const SizedBox(
-                width: 250,
-                child: Image(image: AssetImage('img/nombreImg.png')),
-              ),
-              const Text('\n'),
-              Container(
-                width: 250,
-                height: 285,
-                padding: const EdgeInsets.all(25),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(
-                        color: const Color.fromARGB(221, 134, 134, 134)),
-                    borderRadius: BorderRadius.circular(15)),
-                child: Column(children: [
-                  const TextField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'User Name',
-                          hintText: 'Enter valid mail id as abc@gmail.com')),
-                  const Text('\n'),
-                  const TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Password',
-                        hintText: 'Enter your secure password'),
-                  ),
-                  const Text('\n'),
-                  Container(
-                      width: 200,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.blue,
-                      ),
-                      child: ElevatedButton(
-                        onHover: (value) {
-                          Colors.lightBlue;
-                        },
-                        child: const Text('Iniciar sesión'),
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (_) => const HomePage()));
-                        },
-                      )),
-                ]),
-              ),
-            ],
-          )),
-    ));
-  }
-}
-
-/*
-
-*/
-
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -106,11 +36,118 @@ class HomePage extends StatelessWidget {
           child: ElevatedButton(
               onPressed: () {
                 Navigator.push(context,
-                    MaterialPageRoute(builder: (_) => const Login()));
+                    MaterialPageRoute(builder: (_) => const LoginPage()));
               },
-              child: const Text('Salir')),
+              child: const Text('userName')),
         ),
       ),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class ElPutoUsuario {
+  String? username;
+  String? password;
+
+  ElPutoUsuario(String s, String t);
+}
+
+class _LoginPageState extends State<LoginPage> {
+  ElPutoUsuario elMaylor = ElPutoUsuario("", "");
+
+  Future comprobar() async {
+    final response = await http.post(
+        Uri.parse('http://localhost:8080/auth/login'),
+        headers: {'Content-type': 'application/json'},
+        body: json.encode(
+            {'username': elMaylor.username, 'password': elMaylor.password}));
+
+    if (response.statusCode == 401) {
+      // ignore: use_build_context_synchronously
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(),
+          ));
+    } else {
+      // ignore: use_build_context_synchronously
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MyApp(),
+          ));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(
+                    'https://img.theculturetrip.com/450x/smart/wp-content/uploads/2019/11/gettyimages-643821531.jpg'),
+                fit: BoxFit.cover)),
+        child: Center(
+            heightFactor: 18,
+            child: Column(
+              children: [
+                Container(
+                  width: 250,
+                  margin: const EdgeInsets.all(100),
+                  child: const Image(image: AssetImage('img/nombreImg.png')),
+                ),
+                const Text('\n'),
+                Container(
+                  width: 250,
+                  margin: const EdgeInsets.only(top: 100),
+                  padding: const EdgeInsets.all(25),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Column(children: [
+                    TextFormField(
+                        controller:
+                            TextEditingController(text: elMaylor.username),
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'User Name',
+                            hintText: 'Enter valid mail id as abc@gmail.com')),
+                    const Text('\n'),
+                    TextFormField(
+                      obscureText: true,
+                      controller:
+                          TextEditingController(text: elMaylor.password),
+                      decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Password',
+                          hintText: 'Enter your secure password'),
+                    ),
+                    const Text('\n'),
+                    SizedBox(
+                        width: 200,
+                        child: ElevatedButton(
+                          onHover: (value) {
+                            Colors.lightBlue;
+                          },
+                          child: const Text('Iniciar sesión'),
+                          onPressed: () {
+                            comprobar();
+                          },
+                        )),
+                  ]),
+                ),
+              ],
+            )),
+      )),
     );
   }
 }
